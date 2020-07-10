028a33e9e926b42b2ac80589f90d1c7ea421f56e
horsefall <corrie.ma@protonmail.com>
2020-07-09 23:52:27 -0700

    parse ip address

--- week03/hostscanner/scanner.py
+++ week03/hostscanner/scanner.py
@@ -1,4 +1,7 @@
 import argparse
+import re
+import subprocess
+import sys
 
 class HostScanner():
     def __init__(self, threads, op, ip, save_file):
@@ -7,7 +10,24 @@ class HostScanner():
         self.ip = ip
         self.file = save_file
 
-    def start(self):
+    def start_ping(self):
+        ip_list = []
+        if len(self.ip) == 2:
+            start = self.ip[0].split('.')
+            end = self.ip[1].split('.')
+            new_ip = self.ip[0].split('.')
+            for i in range(int(start[-1]), int(end[-1]) + 1):
+                new_ip[-1] = str(i)
+                ip_list.append('.'.join(new_ip))
+        else:
+            ip_list = self.ip
+
+        for ip in ip_list:
+            pipe = subprocess.Popen("ping -c 3 {}".format(ip), stdout=subprocess.PIPE, shell=True)
+            pipe.wait()
+            print(pipe.poll())
+
+    def start_tcp(self):
         pass
     
     def stop(self):
@@ -23,10 +43,24 @@ def parse_cmd():
     args = parser.parse_args()
 
     # 正则检查ip
-    # 提取ip为列表
-
-    return (args.n, args.f, args.ip, args.w)
+    ip = []
+    regex = r"(2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2}(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}"
+    matches = re.finditer(regex, args.ip, re.MULTILINE)
+    
+    for _, match in enumerate(matches, start=1):
+        ip.append(match.group())
+        if len(ip) > 2:
+            print("ip address error")
+            ip = None
+            break
+    return (args.n, args.f, ip, args.w)
 
 if __name__ == '__main__':
     number, operation, ip, is_write = parse_cmd()
     scanner = HostScanner(number, operation, ip, is_write)
+    if scanner.op == "ping":
+        scanner.start_ping()
+    elif scanner.op == "tcp":
+        scanner.start_tcp()
+    else:
+        print("-f operation error")
