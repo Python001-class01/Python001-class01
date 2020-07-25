3de0cfcb80c7eb5dd3b4b679423846ebc5288fe5
horsefall <h0rs3@horses-MacBook-Pro.local>
2020-07-25 22:06:34 +0800

    fix week04

--- week04/NOTE.md
+++ week04/NOTE.md
@@ -4,3 +4,15 @@
 GROUP BY语句将具有相同值的行分组为摘要行，例如“查找每个国家/地区的客户数量”。
 GROUP BY语句通常与聚合函数（COUNT，MAX，MIN，SUM，AVG）一起使用，以将结果集按一列或多列分组。
 
+#### 作业问题
+
+```sql
+SELECT id,COUNT(DISTINCT order_id) FROM table1 GROUP BY id;
+```
+
+COUNT(DISTINCT order_id) order_id唯一值的数量
+
+```python
+table1.groupby('id').agg({'order_id':pd.Series.nunique()})
+```
+
--- week04/pandas_sql.py
+++ week04/pandas_sql.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 data = pd.DataFrame({
-    "id":[x for x in np.random.randint(500, 1500, 20)],
-    "age":[y for y in np.random.randint(25, 35, 20)],
-    "order_id":[z for z in np.random.randint(1000, 1020, 20)]
-    })
+    "id": [x for x in np.random.randint(500, 1500, 20)],
+    "age": [y for y in np.random.randint(25, 35, 20)],
+    "order_id": [z for z in np.random.randint(1000, 1020, 20)]
+})
 
 # SELECT * FROM data;
 print(data)
@@ -20,31 +20,40 @@ print(data[['id']])
 print(data['id'].count())
 
 # SELECT * FROM data WHERE id<1000 AND age>30;
-print(data[ (data['id'] < 1000) & (data['age'] < 30) ])
+print(data[(data['id'] < 1000) & (data['age'] < 30)])
+
+data2 = pd.DataFrame([
+    {'id': '10000', 'name': 'test1', 'order_id': '11111'},
+    {'id': '10001', 'name': 'test2', 'order_id': '11112'},
+    {'id': '10002', 'name': 'test3', 'order_id': '11112'},
+    {'id': '10002', 'name': 'test4', 'order_id': '11113'},
+    {'id': '10003', 'name': 'test5', 'order_id': '11113'},
+    {'id': '10004', 'name': 'test6', 'order_id': '11113'}
+])
+# SELECT id,COUNT(DISTINCT order_id) FROM table1 GROUP BY id;
+data2.groupby('id').agg({'order_id': 'nunique'})
 
 table1 = pd.DataFrame({
-    "id":[x for x in np.random.randint(500, 1500, 20)],
-    "age":[y for y in np.random.randint(25, 35, 20)],
-    "order_id":[z for z in np.random.randint(1000, 1020, 20)]
-    })
+    "id": [x for x in np.random.randint(500, 1500, 20)],
+    "age": [y for y in np.random.randint(25, 35, 20)],
+    "order_id": [z for z in np.random.randint(1000, 1020, 20)]
+})
 
 table2 = pd.DataFrame({
-    "id":[x for x in np.random.randint(500, 1500, 20)],
-    "age":[y for y in np.random.randint(25, 35, 20)],
-    "order_id":[z for z in np.random.randint(1000, 1020, 20)]
-    })
-
-# SELECT id,COUNT(DISTINCT order_id) FROM table1 GROUP BY id;
-table1.groupby('id').agg({'order_id':pd.Series.unique})
+    "id": [x for x in np.random.randint(500, 1500, 20)],
+    "age": [y for y in np.random.randint(25, 35, 20)],
+    "order_id": [z for z in np.random.randint(1000, 1020, 20)]
+})
 
 # SELECT * FROM table1 t1 INNER_JOIN table2 t2 ON t1.id = t2.id;
 pd.merge(table1, table2, on='id')
 
 # SELECT * FROM table1 UNION SELECT * FROM table2;
-pd.concat([table1, table2])
- 
+pd.concat([table1, table2]).drop_duplicates()
+
 # DELETE FROM table1 WHERE id=10;
-table1 = table1[table1['id'] == 10]
+del_index = table1[table1['id'] == 10].index
+table1.drop(index, axis=0)
 
 # ALTER TABLE table1 DROP COLUMN column_name;
-table1.drop(columns = ['id'], axis = 1)
+table1.drop(columns=['id'], axis=1)
